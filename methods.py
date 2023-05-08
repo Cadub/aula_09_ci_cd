82beb999f1014a2dd44a63d6437e07d25bc7e934
cadub <cadubas1@gmail.com>
2023-05-08 08:32:18 -0300

    Adicionando Calculo do  Volume Paralelepipedo e Função de teste

--- methods.py
+++ methods.py
@@ -5,3 +5,7 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def volume_of_parallelepiped(length, width, height):
+    volume = length*width*height
+    return volume
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,15 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_volume():
+    # given a lenght of 3, a width of 2 and a height of 5
+    length = 3
+    width = 2
+    height = 5
+
+    # when we calculate the perimeter
+    output = methods.volume_of_parallelepiped(length, width, height)
+    
+    # then the perimeter should be 30
+    assert output == 30
